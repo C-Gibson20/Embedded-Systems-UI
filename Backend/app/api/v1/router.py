534a33ebf897861f048306eb3755feef47025087
C-Gibson20 <117122578+C-Gibson20@users.noreply.github.com>
2026-01-27 18:26:53 +0000

    router fix

--- Backend/app/api/v1/router.py
+++ Backend/app/api/v1/router.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter
-from sympy import capture
-from app.api.v1.endpoints import health, analyze, upload, capture, result, stored_image
+from app.api.v1.endpoints import health, analyze, upload, capture, result, stored_image, pi_ws
 
 router = APIRouter()
 
@@ -10,4 +9,5 @@ router.include_router(health.router, tags=["health"])
 router.include_router(capture.router, tags=["capture"])
 router.include_router(result.router, tags=["result"])
 router.include_router(upload.router, tags=["pi"])
+router.include_router(pi_ws.router, tags=["pi-ws"])
 router.include_router(stored_image.router, tags=["pi-image"])
